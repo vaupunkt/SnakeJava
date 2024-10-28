f5db19f802d3e261b9855f03fe33ab4d7326194b
Veliko Kardziev <velikok@gmail.com>
2024-10-28 14:54:47 +0100

    Refactor game panel draw method and add game over functionality

--- snake/src/GamePanel.java
+++ snake/src/GamePanel.java
@@ -53,24 +53,27 @@ public class GamePanel extends JPanel implements ActionListener {
     }
     
     public void draw(Graphics g) {
-
-        for (int i = 0; i<SCREEN_HEIGHT/UNIT_SIZE; i++) {
-            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
-        }
-
-        g.setColor(Color.red);
-        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-
-        for (int i = 0; i < bodyParts; i++) {
-            if (i == 0) {
-                g.setColor(Color.green);
-                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-            } else {
-                g.setColor(new Color(45, 180, 0));
-                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+        if (running) {
+            for (int i = 0; i<SCREEN_HEIGHT/UNIT_SIZE; i++) {
+                g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+                g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
             }
-        }
+            
+            g.setColor(Color.red);
+            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+            
+            for (int i = 0; i < bodyParts; i++) {
+                if (i == 0) {
+                    g.setColor(Color.green);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                } else {
+                    g.setColor(new Color(45, 180, 0));
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+            }
+        }  else {
+            gameOver(g);
+        } 
     }
 
     public void move() {
@@ -80,31 +83,67 @@ public class GamePanel extends JPanel implements ActionListener {
         }
 
         switch (direction) {
-            case 'R':
-                x[0] = x[0] + UNIT_SIZE;
-            case 'U':
-                y[0] = y[0] - UNIT_SIZE;
-            case 'L':
-                x[0] = x[0] - UNIT_SIZE;
-            case 'D':
-                y[0] = y[0] + UNIT_SIZE;
+            case 'R' -> x[0] = x[0] + UNIT_SIZE;
+            case 'U' -> y[0] = y[0] - UNIT_SIZE;
+            case 'L' -> x[0] = x[0] - UNIT_SIZE;
+            case 'D' -> y[0] = y[0] + UNIT_SIZE;
         }
     }
 
     public void checkApple() {
-
+        if ((x[0] == appleX) && (y[0] == appleY)) {
+            bodyParts++;
+            applesEaten++;
+            System.out.println("Apples eaten: " + applesEaten);
+            newApple();
+        }
     }
 
     public void checkCollisions() {
+        // check if head collides with body
+        for (int i = bodyParts; i > 0; i--){
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+            }
+        }
+
+        // check if head touches left border
+        if (x[0] < 0) {
+            running = false;
+        }
+
+        // check if head touches right border
+        if (x[0] > SCREEN_WIDTH) {
+            running = false;
+        }
+
+        // check if head touches top border
+        if (y[0] < 0) {
+            running = false;
+        }
+
+        // check if head touches bottom border
+        if (y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
+
+        if (!running) {
+            timer.stop();
+        }
 
     }
 
     public void gameOver(Graphics g) {
-
+        // Game Over Text
+        g.setColor(Color.red);
+        g.setFont(new Font("Ink Free", Font.BOLD, 75));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Game Over", (SCREEN_WIDTH - metrics.stringWidth("Game Over")) / 2, SCREEN_HEIGHT / 2);
     }
     
     @Override
     public void actionPerformed(ActionEvent e) {
+   
         if (running) {
             move();
             checkApple();
@@ -116,6 +155,31 @@ public class GamePanel extends JPanel implements ActionListener {
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if (direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if (direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+
+                case KeyEvent.VK_UP:
+                    if (direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
+
+                case KeyEvent.VK_DOWN:
+                    if (direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+
+            }
 
         }
     }
